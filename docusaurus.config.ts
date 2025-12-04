561b8ed8db20d1248447aff6de06beeef6724c55
jhqwqmc <jhcraft@qq.com>
2025-12-05 03:34:44 +0800

    修复？

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,17 +19,18 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://momirealms.net',
+  url: 'https://wiki.momirealms.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/craft-engine-wiki/',
+  baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Xiao-MoMi', // Usually your GitHub org/user name.
   projectName: 'craft-engine-wiki', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   markdown: {
     hooks: {
